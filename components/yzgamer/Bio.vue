95bf30ad86db0cf870a95ef7f92b148bdb671bad
柚子 <loremmlel@163.com>
2025-02-16 00:03:30 +0800

    feat(user): 添加用户昵称和签名编辑功能
    - 新增 Bio 组件，用于编辑用户签名- 新增 Username 组件，用于更改用户名
    - 实现了签名和用户名的后端更新逻辑- 添加了相关的国际化文案和提示信息

--- components/yzgamer/Bio.vue
+++ components/yzgamer/Bio.vue
@@ -1,11 +1,61 @@
 <script setup lang="ts">
+import type {UserInfo} from "~/types/api/user";
+import {InfoCode} from "~/code&message/infoCode";
 
+const {t} = useI18n()
+
+const props = defineProps<{ user: UserInfo }>()
+
+const refresh = inject<() => Promise<void>>('refresh')
+const bioValue = ref('')
+
+async function handleChangeBio() {
+  if (!bioValue.value.trim()) {
+    return
+  }
+
+  if (bioValue.value.length > 107) {
+    useMessage(InfoCode.SignatureLengthExceeded, 'warn')
+    return
+  }
+
+  const result = await $fetch('/api/user/bio', {
+    method: 'PUT',
+    watch: false,
+    body: {bio: bioValue.value},
+    ...yzforumResponseHandler
+  })
+
+  if (result) {
+    useMessage(InfoCode.SignatureUpdateSuccess, 'success')
+    bioValue.value = ''
+    await refresh?.()
+  }
+}
+
+onMounted(() => {
+  bioValue.value = props.user.bio
+})
 </script>
 
 <template>
+  <div class="bio">
+    <div class="title">{{ t('user.settings.bio') }}</div>
+    <YuzuTextarea name="bio" :placeholder="`${t('user.settings.hint')}`" rows="5" v-model="bioValue"></YuzuTextarea>
 
+    <YuzuButton @click="handleChangeBio">
+      {{ t('user.settings.confirm') }}
+    </YuzuButton>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.bio {
+  width: 100%;
+  margin-bottom: 20px;
+}
 
+.title {
+  margin-bottom: 10px;
+}
 </style>
--- components/yzgamer/Username.vue
+++ components/yzgamer/Username.vue
@@ -1,11 +1,62 @@
 <script setup lang="ts">
+import {InfoCode} from "~/code&message/infoCode";
 
+const {t} = useI18n()
+
+const message = useComponentMessageStore()
+
+const refresh = inject<() => Promise<void>>('refresh')
+const inputValue = ref('')
+
+async function handleChangeUsername() {
+  if (!isValidName(inputValue.value)) {
+    useMessage(InfoCode.InvalidUsername, 'warn')
+    return
+  }
+
+  const res = await message.alert({
+    'en-us': 'Are you sure you want to change your username? This will cost you 17 points.',
+    'ja-jp': 'ユーザー名を変更してもよろしいですか？これには 17 ポイントが必要です。',
+    'zh-cn': '您确定更改用户名吗？这将会消耗您 17 柚子点',
+  })
+  if (!res) {
+    return
+  }
+
+  useMessage(InfoCode.UsernameUpdating, 'info')
+  const result = await $fetch('/api/user/username', {
+    method: 'PUT',
+    watch: false,
+    body: {username: inputValue.value},
+    ...yzforumResponseHandler
+  })
+
+  if (result) {
+    useMessage(InfoCode.UsernameUpdateSuccess, 'success')
+    await refresh?.()
+  }
+}
 </script>
 
 <template>
-
+  <div class="username">
+    <div class="title">{{ t('user.settings.username') }}</div>
+    <p>{{ t('user.settings.usernameHint') }}</p>
+    <YuzuInput type="text" v-model="inputValue"></YuzuInput>
+    <YuzuButton @click="handleChangeUsername">
+      {{ t('user.settings.confirm') }}
+    </YuzuButton>
+  </div>
 </template>
 
 <style scoped lang="scss">
+p {
+  margin: 10px 0;
+  font-size: 14px;
+}
 
+input {
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
 </style>
