eee9fdaf27c6b18aeb8124bc23737eb2bb71a9fb
柚子 <loremmlel@163.com>
2025-03-10 15:33:21 +0800

    feat(search): 添加搜索历史和结果展示功能
    - 实现搜索历史的显示、点击和删除功能
    - 添加清除搜索历史的功能
    - 实现不同类型的搜索结果展示，包括主题、游戏、用户、回复和评论
    - 优化搜索结果的样式和布局

--- components/search/History.vue
+++ components/search/History.vue
@@ -1,11 +1,109 @@
 <script setup lang="ts">
+const {t} = useI18n()
 
+const persistSearchStore = usePersistYzforumSearchStore()
+const searchStore = useTempSearchStore()
+
+function handleClickHistory(index: number) {
+  searchStore.keywords = persistSearchStore.searchHistory[index]
+}
+
+function clearSearchHistory() {
+  persistSearchStore.searchHistory = []
+}
+
+function handleDeleteHistory(index: number) {
+  persistSearchStore.searchHistory.splice(index, 1)
+}
 </script>
 
 <template>
+  <div class="history">
+    <div class="title" v-if="persistSearchStore.searchHistory.length">
+      <span>{{ t('search.history') }}</span>
+      <span @click="clearSearchHistory">
+        {{ t('search.clear') }}
+      </span>
+    </div>
 
+    <div class="history-container" v-if="persistSearchStore.searchHistory.length">
+      <div class="single-history" v-for="(history, index) in persistSearchStore.searchHistory"
+           @click="handleClickHistory(index)">
+        <span>{{ history }} </span>
+        <span class="delete">
+          <Icon class="icon" @click="handleDeleteHistory(index)" name="lucide:x"></Icon>
+        </span>
+      </div>
+    </div>
+
+    <YuzuNull :condition="!persistSearchStore.searchHistory.length" type="null"></YuzuNull>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.history {
+  width: 100%;
+  margin-top: 20px;
+}
 
+.title {
+  display: flex;
+  justify-content: space-between;
+
+  span {
+    font-size: 16px;
+    &:nth-child(2) {
+      cursor: pointer;
+
+      &:hover {
+        color: var(--yzforum-blue-5);
+      }
+    }
+  }
+}
+
+.history-container {
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  margin-top: 10px;
+}
+
+.single-history {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px;
+  margin: 2px 0;
+  border-radius: 10px;
+
+  &:hover {
+    color: var(--yzforum-blue-5);
+    background-color: var(--yzforum-trans-blue-1);
+
+    .delete {
+      display: flex;
+    }
+  }
+
+  span:nth-child(1) {
+    cursor: default;
+    position: relative;
+    display: flex;
+    overflow: hidden;
+  }
+
+  span:nth-child(2) {
+    width: 20px;
+  }
+}
+
+.delete {
+  font-size: 20px;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  color: var(--yzforum-font-color-0);
+  display: none;
+}
 </style>
--- components/search/Result.vue
+++ components/search/Result.vue
@@ -1,11 +1,70 @@
 <script setup lang="ts">
+import type {
+  SearchResult, SearchResultComment,
+  SearchResultGame,
+  SearchResultReply,
+  SearchResultTopic,
+  SearchResultUser,
+  SearchType
+} from "~/types/api/search";
 
+const persistSearchStore = usePersistYzforumSearchStore()
+const searchStore = useTempSearchStore()
+
+const props = defineProps<{
+  results: SearchResult[],
+  type: SearchType
+}>()
+
+function handleClick() {
+  if (!persistSearchStore.searchHistory.includes(searchStore.keywords)) {
+    persistSearchStore.searchHistory.unshift(searchStore.keywords)
+  }
+}
 </script>
 
 <template>
+  <div class="result">
+    <div class="container" v-if="props.type === 'topic'">
+      <div v-for="topic in results">
+        <HomeTopicCard @click="handleClick" :topic="topic as SearchResultTopic"></HomeTopicCard>
+        <YuzuDivider margin="0 10px"></YuzuDivider>
+      </div>
+    </div>
 
+    <div class="container" v-else-if="props.type === 'game'">
+      <div v-for="game in results">
+        <HomeGameCard @click="handleClick" :game="game as SearchResultGame"></HomeGameCard>
+        <YuzuDivider margin="0 10px"></YuzuDivider>
+      </div>
+    </div>
+
+    <div class="container" v-else-if="props.type === 'user'">
+      <div v-for="user in results">
+        <SearchUserCard :user="user as SearchResultUser"></SearchUserCard>
+        <YuzuDivider margin="0 20px"></YuzuDivider>
+      </div>
+    </div>
+
+    <div class="container" v-else-if="props.type === 'reply'">
+      <div v-for="reply in results">
+        <SearchReplyCommentCard :data="reply as SearchResultReply" type="reply"></SearchReplyCommentCard>
+        <YuzuDivider margin="0 20px"></YuzuDivider>
+      </div>
+    </div>
+
+    <div class="container" v-else-if="props.type === 'comment'">
+      <div v-for="comment in results">
+        <SearchReplyCommentCard :data="comment as SearchResultComment" type="comment"></SearchReplyCommentCard>
+        <YuzuDivider margin="0 20px"></YuzuDivider>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-
+.result {
+  width: 100%;
+  margin-top: 20px;
+}
 </style>
