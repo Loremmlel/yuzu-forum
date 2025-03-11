75db4d445a06a5fb8251fda6bf51c7dfea8e3a10
柚子 <loremmlel@163.com>
2025-03-11 13:32:23 +0800

    fix:修复移动端下 List组件某些文本可能溢出的问题- 在 List 组件的样式中添加了 word-break 和 white-space属性 - 确保所有子元素都能正确处理文本换行，防止溢出

--- components/yzgamer/List.vue
+++ components/yzgamer/List.vue
@@ -14,6 +14,12 @@
   padding: 10px 20px;
   display: flex;
   flex-direction: column;
+
+  // fix: 修复移动端下某些文本可能溢出的问题
+  * {
+    word-break: break-all;
+    white-space: pre-wrap;
+  }
 }
 
 @media (max-width: 700px) {
